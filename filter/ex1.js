67734735ac461b795f70b9ea1c5dfdc20980931c
Celine Berthelin <celine.berthelin@gmail.com>
2019-10-04 10:47:42 +0200

    Exercice 1 et 3 filter

--- filter/ex1.js
+++ filter/ex1.js
@@ -10,13 +10,17 @@ Sortie attendue:
   [7, 2, 0, 3, 11]
 
  */
-
+const number = [7, -4, 2, 0, -10, 3, 11];
 function getPositiveNumbers(numbers) {
+return numbers.filter (function (num) {
+  return num >= 0
+});
 }
-
+  
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
 // console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
 
 // Ne pas modifier l'export
 module.exports = getPositiveNumbers;
+
